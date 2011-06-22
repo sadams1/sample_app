6d4afa8a1184ada87a813a6e579f2b01437c4d93
Sean Adams <sean_adams@intuit.com>
2011-06-22 09:03:12 -0700

    added helper and stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,6 +4,10 @@ describe PagesController do
 
   render_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -13,7 +17,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => "#{@base_title}| Home")
     end
   end
 
@@ -25,7 +29,7 @@ describe PagesController do
        it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => "#{@base_title} | Contact")
     end
   end
 
@@ -37,7 +41,7 @@ describe PagesController do
         it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | About")
+                                    :content => "#{@base_title} | About")
     end
   end
 
